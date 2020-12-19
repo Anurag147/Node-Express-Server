8fc572ea38c88757749be39556de3cbf1f393d75
Anurag <a.asthana90@gmail.com>
2020-12-19 20:19:39 +0530

    google changes

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //Load Express server
+const passport = require('passport'); //Load passport for oAuth
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //Load google oauth strategy
+const keys = require('./config/keys'); //Load keys from configuration file
+const app = express();//Create instance of express server
 
-app.get('/',(req,res)=>{
-    res.send({hi:'express server called'});
-});
+//Assign passport to use google strategy for oAuth
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL:'/auth/google/callback'
+    },
+    (accessToken,refreshToken,profile,done)=>
+    {
+        console.log(accessToken);//Log access token recieved
+        console.log(refreshToken);//Log refresh token recieved
+        console.log(profile);//Log profile recieved 
+    }
+));
+
+//Handle google authentication 
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}));
+
+//Handle google authentication callback 
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
